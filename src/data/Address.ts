542f8429d0dbf804e141e185f031279ce0f4019a
Pascal Allen <pascal.allen88@gmail.com>
2021-09-26 09:11:24 -0500

    update readme, functionality (#1)

---
+++ src/data/Address.ts
@@ -0,0 +1,9 @@
+export type Address = {
+  buildingName: string;
+  streetLine1: string;
+  streetLine2: string;
+  city: string;
+  stateProvince: string;
+  zipPostalCode: string;
+  country: string;
+};
