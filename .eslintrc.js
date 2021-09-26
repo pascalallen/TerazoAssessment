6e843fea4319d0864945bf3ff32918f0aa259d1c
Pascal <pascal.allen88@gmail.com>
2021-09-26 09:09:07 -0500

    update readme, functionality

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,34 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       modules: true,
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   rules: {
-    "react/prop-types": 0,
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "@typescript-eslint/camelcase": 0,
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    'react/prop-types': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        args: "after-used",
+        args: 'after-used',
         ignoreRestSiblings: true,
-        vars: "all",
-      },
+        vars: 'all'
+      }
     ],
-    "@typescript-eslint/no-empty-function": 0,
+    '@typescript-eslint/no-empty-function': 0,
+    'prettier/prettier': ['error', { singleQuote: true }]
   },
-  ignorePatterns: ["node_modules/"],
+  ignorePatterns: ['node_modules/'],
   settings: {
     react: {
-      version: "detect",
-    },
-  },
+      version: 'detect'
+    }
+  }
 };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,28 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { Table, Button } from "reactstrap";
-import axios from "axios";
+import _ from 'lodash';
+import React, { useEffect, useState } from 'react';
+import { Table, Button } from 'reactstrap';
+import axios from 'axios';
+import { Warehouse } from '@/data/Warehouse';
+import { Factory } from '@/data/Factory';
 
 type State = {
-  warehouses: any[];
+  warehouses: Warehouse[];
+  factories: Factory[];
 };
 
 const initialState: State = {
   warehouses: [],
+  factories: []
 };
 
 const Home = (): React.ReactElement => {
   const [warehouses, setWarehouses] = useState(initialState.warehouses);
-
-  // state = {
-  //   warehouses: [],
-  // };
-
-  // componentWillMount() {
-  //   axios.get("http://localhost:3000/warehouses");
-  // }
+  const [factories, setFactories] = useState(initialState.factories);
 
   useEffect(() => {
-    axios.get(" http://localhost:3000/warehouses");
+    axios.get('http://localhost:3000/warehouses.json').then(response => {
+      setWarehouses(response.data);
+    });
+
+    axios.get('http://localhost:3000/factories.json').then(response => {
+      setFactories(response.data);
+    });
   }, []);
 
   return (
@@ -30,26 +34,76 @@ const Home = (): React.ReactElement => {
       <Table>
         <thead>
           <tr>
-            <th>Heading</th>
-            <th>Heading</th>
-            <th>Heading</th>
+            <th>Type</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Address</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td />
-            <td />
-            <td />
-            <td>
-              <Button color="success" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button color="danger" size="sm">
-                Delete
-              </Button>
-            </td>
-          </tr>
+          {_.map(warehouses, (warehouse: Warehouse) => (
+            <tr key={`warehouse-row-${warehouse.warehouseId}`}>
+              <td>Warehouse</td>
+              <td>{warehouse.warehouseName}</td>
+              <td width={500}>{warehouse.warehouseDescription}</td>
+              <td>
+                {warehouse.warehouseAddress.buildingName +
+                  ' ' +
+                  warehouse.warehouseAddress.streetLine1 +
+                  ' ' +
+                  warehouse.warehouseAddress.streetLine2}
+                <br />
+                {warehouse.warehouseAddress.city +
+                  ' ' +
+                  warehouse.warehouseAddress.stateProvince +
+                  ' ' +
+                  warehouse.warehouseAddress.zipPostalCode +
+                  ' ' +
+                  warehouse.warehouseAddress.country +
+                  ' '}
+              </td>
+              <td>
+                <Button color="success" size="sm" className="mr-2">
+                  Edit
+                </Button>
+                <Button color="danger" size="sm">
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+          {_.map(factories, (factory: Factory) => (
+            <tr key={`factory-row-${factory.factoryId}`}>
+              <td>Factory</td>
+              <td>{factory.factoryName}</td>
+              <td width={500}>{factory.factoryDescription}</td>
+              <td>
+                {factory.factoryAddress.buildingName +
+                  ' ' +
+                  factory.factoryAddress.streetLine1 +
+                  ' ' +
+                  factory.factoryAddress.streetLine2}
+                <br />
+                {factory.factoryAddress.city +
+                  ' ' +
+                  factory.factoryAddress.stateProvince +
+                  ' ' +
+                  factory.factoryAddress.zipPostalCode +
+                  ' ' +
+                  factory.factoryAddress.country +
+                  ' '}
+              </td>
+              <td>
+                <Button color="success" size="sm" className="mr-2">
+                  Edit
+                </Button>
+                <Button color="danger" size="sm">
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
